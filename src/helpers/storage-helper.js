b5a366316d6509881d12d9e84ce9665bb3aefcd0
Thomas Wallace <thomas@xmartlabs.com>
2021-04-06 20:04:23 -0300

    Catch localStorage errors

--- src/helpers/storage-helper.js
+++ src/helpers/storage-helper.js
@@ -1,11 +1,15 @@
 // Uses localStorage as default, could be changed to indexedDb or other solution if needed
 class Storage {
   static store(key, value) {
-    window.localStorage.setItem(key, value);
+    try {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (e) { return false; }
   }
 
   static retrieve(key) {
-    return window.localStorage.getItem(key);
+    try {
+      return JSON.parse(window.localStorage.getItem(key));
+    } catch (e) { return false; }
   }
 }
 
